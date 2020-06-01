41b375e9182aabe1223d2c8706bbf7808a89f694
Ricky <40550552+refloresmaynasa@users.noreply.github.com>
2020-06-01 03:08:29 -0400

    test changes from github
    .github/workflows/spring-boot/maven.yml

--- spring-boot/src/main/java/ricky/practices/springboot/controller/HelloWorldController.java
+++ spring-boot/src/main/java/ricky/practices/springboot/controller/HelloWorldController.java
@@ -13,6 +13,6 @@ public class HelloWorldController {
 
     @RequestMapping({"/hello"})
     public String firstPage() {
-        return "Hello World Ricky";
+        return "Hello World Ricky ...";
     }
 }
